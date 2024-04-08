d300b86d0750f6666bd5fac4556e2420a25ba032
Felipe Baptista <felipe.baptista06@gmail.com>
2024-04-08 18:19:07 -0300

    add images in studio

---
+++ src/assets/index.js
@@ -0,0 +1,330 @@
+import BancoExterno001Front from './imgs/Banco Externo 001 - Front.png';
+import BancoExterno001Persp from './imgs/Banco Externo 001 - Persp.png';
+import BancoExterno001Side from './imgs/Banco Externo 001 - Side.png';
+import BancoExterno001Top from './imgs/Banco Externo 001 - Top.png';
+import BancoExterno002Front from './imgs/Banco Externo 002 - Front.png';
+import BancoExterno002Persp from './imgs/Banco Externo 002 - Persp.png';
+import BancoExterno002Side from './imgs/Banco Externo 002 - Side.png';
+import BancoExterno002Top from './imgs/Banco Externo 002 - Top.png';
+import BancoExterno003Front from './imgs/Banco Externo 003 - Front.png';
+import BancoExterno003Persp from './imgs/Banco Externo 003 - Persp.png';
+import BancoExterno003Side from './imgs/Banco Externo 003 - Side.png';
+import BancoExterno003Top from './imgs/Banco Externo 003 - Top.png';
+import BancoExterno004Front from './imgs/Banco Externo 004 - Front.png';
+import BancoExterno004Persp from './imgs/Banco Externo 004 - Persp.png';
+import BancoExterno004Side from './imgs/Banco Externo 004 - Side.png';
+import BancoExterno004Top from './imgs/Banco Externo 004 - Top.png';
+import BancoExterno005Front from './imgs/Banco Externo 005 - Front.png';
+import BancoExterno005Persp from './imgs/Banco Externo 005 - Persp.png';
+import BancoExterno005Side from './imgs/Banco Externo 005 - Side.png';
+import BancoExterno005Top from './imgs/Banco Externo 005 - Top.png';
+import Bike001Front from './imgs/Bike 001 - Front.png';
+import Bike001Persp from './imgs/Bike 001 - Persp.png';
+import Bike001Side from './imgs/Bike 001 - Side.png';
+import Bike001Top from './imgs/Bike 001 - Top.png';
+import CalligarisSwingFront from './imgs/Calligaris Swing - Front.png';
+import CalligarisSwingPersp from './imgs/Calligaris Swing - Persp.png';
+import CalligarisSwingSide from './imgs/Calligaris Swing - Side.png';
+import CalligarisSwingTop from './imgs/Calligaris Swing - Top.png';
+import Cooktop001Front from './imgs/Cooktop 001 - Front.png';
+import Cooktop001Persp from './imgs/Cooktop 001 - Persp.png';
+import Cooktop001Side from './imgs/Cooktop 001 - Side.png';
+import Cooktop001Top from './imgs/Cooktop 001 - Top.png';
+import Cooktop002Front from './imgs/Cooktop 002 - Front.png';
+import Cooktop002Persp from './imgs/Cooktop 002 - Persp.png';
+import Cooktop002Side from './imgs/Cooktop 002 - Side.png';
+import Cooktop002Top from './imgs/Cooktop 002 - Top.png';
+import DepuradorDeAr001Front from './imgs/Depurador de ar 001 - Front.png';
+import DepuradorDeAr001Persp from './imgs/Depurador de ar 001 - Persp.png';
+import DepuradorDeAr001Side from './imgs/Depurador de ar 001 - Side.png';
+import DepuradorDeAr001Top from './imgs/Depurador de ar 001 - Top.png';
+import EmmegiEm202MeshFront from './imgs/Emmegi - EM202 Mesh - Front.png';
+import EmmegiEm202MeshPersp from './imgs/Emmegi - EM202 Mesh - Persp.png';
+import EmmegiEm202MeshSide from './imgs/Emmegi - EM202 Mesh - Side.png';
+import EmmegiEm202MeshTop from './imgs/Emmegi - EM202 Mesh - Top.png';
+import FlexFormFront from './imgs/Flexform Victor Large - Front.png';
+import FlexFormPersp from './imgs/Flexform Victor Large - Persp.png';
+import FlexFormSide from './imgs/Flexform Victor Large - Side.png';
+import FlexFormTop from './imgs/Flexform Victor Large - Top.png';
+import Fogao001Front from './imgs/Fogão 001 - Front.png';
+import Fogao001Persp from './imgs/Fogão 001 - Persp.png';
+import Fogao001Side from './imgs/Fogão 001 - Side.png';
+import Fogao001Top from './imgs/Fogão 001 - Top.png';
+import Forno001Front from './imgs/Forno 001 - Front.png';
+import Forno001Persp from './imgs/Forno 001 - Persp.png';
+import Forno001Side from './imgs/Forno 001 - Side.png';
+import Forno001Top from './imgs/Forno 001 - Top.png';
+import Forno002Front from './imgs/Forno 002 - Front.png';
+import Forno002Persp from './imgs/Forno 002 - Persp.png';
+import Forno002Side from './imgs/Forno 002 - Side.png';
+import Forno002Top from './imgs/Forno 002 - Top.png';
+import JogoDeMesaECadeiras001Front from './imgs/Jogo Mesa e Cadeiras 001 - Front.png';
+import JogoDeMesaECadeiras001Persp from './imgs/Jogo Mesa e Cadeiras 001 - Persp.png';
+import JogoDeMesaECadeiras001Side from './imgs/Jogo Mesa e Cadeiras 001 - Side.png';
+import JogoDeMesaECadeiras001Top from './imgs/Jogo Mesa e Cadeiras 001 - Top.png';
+import JogoDeMesaECadeiras002Front from './imgs/Jogo Mesas e Cadeiras 002 - Front.png';
+import JogoDeMesaECadeiras002Persp from './imgs/Jogo Mesas e Cadeiras 002 - Persp.png';
+import JogoDeMesaECadeiras002Side from './imgs/Jogo Mesas e Cadeiras 002 - Side.png';
+import JogoDeMesaECadeiras002Top from './imgs/Jogo Mesas e Cadeiras 002 - Top.png';
+import JogoDeMesaECadeiras003Front from './imgs/Jogo Mesa e Cadeiras 003 - Front.png';
+import JogoDeMesaECadeiras003Persp from './imgs/Jogo Mesa e Cadeiras 003 - Persp.png';
+import JogoDeMesaECadeiras003Side from './imgs/Jogo Mesa e Cadeiras 003 - Side.png';
+import JogoDeMesaECadeiras003Top from './imgs/Jogo Mesa e Cadeiras 003 - Top.png';
+import JogoDeMesaECadeiras004Front from './imgs/Jogo Mesa e Cadeiras 004 - Front.png';
+import JogoDeMesaECadeiras004Persp from './imgs/Jogo Mesa e Cadeiras 004 - Persp.png';
+import JogoDeMesaECadeiras004Side from './imgs/Jogo Mesa e Cadeiras 004 - Side.png';
+import JogoDeMesaECadeiras004Top from './imgs/Jogo Mesa e Cadeiras 004 - Top.png';
+import LavaLoucas001Front from './imgs/Lava louças 001 - Front.png';
+import LavaLoucas001Persp from './imgs/Lava louças 001 - Persp.png';
+import LavaLoucas001Side from './imgs/Lava louças 001 - Side.png';
+import LavaLoucas001Top from './imgs/Lava louças 001 - Top.png';
+import LavaRoupa001Front from './imgs/Lava Roupa 001 - Front.png';
+import LavaRoupa001Persp from './imgs/Lava Roupa 001 - Persp.png';
+import LavaRoupa001Side from './imgs/Lava Roupa 001 - Side.png';
+import LavaRoupa001Top from './imgs/Lava Roupa 001 - Top.png';
+import LivingDivaniNeowallFront from './imgs/Living Divani - Neowall - Front.png';
+import LivingDivaniNeowallPersp from './imgs/Living Divani - Neowall - Persp.png';
+import LivingDivaniNeowallSide from './imgs/Living Divani - Neowall - Side.png';
+import LivingDivaniNeowallTop from './imgs/Living Divani - Neowall - Top.png';
+import LivingDivaniFrogLoungeFront from './imgs/Living Divani Frog Lounge - Front.png';
+import LivingDivaniFrogLoungePersp from './imgs/Living Divani Frog Lounge - Persp.png';
+import LivingDivaniFrogLoungeSide from './imgs/Living Divani Frog Lounge - Side.png';
+import LivingDivaniFrogLoungeTop from './imgs/Living Divani Frog Lounge - Top.png';
+import MDFItaliaEdgar2Front from './imgs/MDF Italia Edgar II - Front.png';
+import MDFItaliaEdgar2Persp from './imgs/MDF Italia Edgar II - Persp.png';
+import MDFItaliaEdgar2Side from './imgs/MDF Italia Edgar II - Side.png';
+import MDFItaliaEdgar2Top from './imgs/MDF Italia Edgar II - Top.png';
+import MDFItaliaYaleFront from './imgs/MDF Italia Yale - Front.png';
+import MDFItaliaYalePersp from './imgs/MDF Italia Yale - Persp.png';
+import MDFItaliaYaleSide from './imgs/MDF Italia Yale - Side.png';
+import MDFItaliaYaleTop from './imgs/MDF Italia Yale - Top.png';
+import MiniRefrigerador001Front from './imgs/Mini Refrigerador 001 - Front.png';
+import MiniRefrigerador001Persp from './imgs/Mini Refrigerador 001 - Persp.png';
+import MiniRefrigerador001Side from './imgs/Mini Refrigerador 001 - Side.png';
+import MiniRefrigerador001Top from './imgs/Mini Refrigerador 001 - Top.png';
+import MiniRefrigerador002Front from './imgs/Mini Refrigerador 002 - Front.png';
+import MiniRefrigerador002Persp from './imgs/Mini Refrigerador 002 - Persp.png';
+import MiniRefrigerador002Side from './imgs/Mini Refrigerador 002 - Side.png';
+import MiniRefrigerador002Top from './imgs/Mini Refrigerador 002 - Top.png';
+
+export const allImages = [
+    BancoExterno001Front,
+    BancoExterno001Persp,
+    BancoExterno001Side,
+    BancoExterno001Top,
+    BancoExterno002Front,
+    BancoExterno002Persp,
+    BancoExterno002Side,
+    BancoExterno002Top,
+    BancoExterno003Front,
+    BancoExterno003Persp,
+    BancoExterno003Side,
+    BancoExterno003Top,
+    BancoExterno004Front,
+    BancoExterno004Persp,
+    BancoExterno004Side,
+    BancoExterno004Top,
+    BancoExterno005Front,
+    BancoExterno005Persp,
+    BancoExterno005Side,
+    BancoExterno005Top,
+    Bike001Front,
+    Bike001Persp,
+    Bike001Side,
+    Bike001Top,
+    CalligarisSwingFront,
+    CalligarisSwingPersp,
+    CalligarisSwingSide,
+    CalligarisSwingTop,
+    Cooktop001Front,
+    Cooktop001Persp,
+    Cooktop001Side,
+    Cooktop001Top,
+    Cooktop002Front,
+    Cooktop002Persp,
+    Cooktop002Side,
+    Cooktop002Top,
+    DepuradorDeAr001Front,
+    DepuradorDeAr001Persp,
+    DepuradorDeAr001Side,
+    DepuradorDeAr001Top,
+    EmmegiEm202MeshFront,
+    EmmegiEm202MeshPersp,
+    EmmegiEm202MeshSide,
+    EmmegiEm202MeshTop,
+    FlexFormFront,
+    FlexFormPersp,
+    FlexFormSide,
+    FlexFormTop,
+    JogoDeMesaECadeiras001Front,
+    JogoDeMesaECadeiras001Persp,
+    JogoDeMesaECadeiras001Side,
+    JogoDeMesaECadeiras001Top,
+    JogoDeMesaECadeiras002Front,
+    JogoDeMesaECadeiras002Persp,
+    JogoDeMesaECadeiras002Side,
+    JogoDeMesaECadeiras002Top,
+    JogoDeMesaECadeiras003Front,
+    JogoDeMesaECadeiras003Persp,
+    JogoDeMesaECadeiras003Side,
+    JogoDeMesaECadeiras003Top,
+    JogoDeMesaECadeiras004Front,
+    JogoDeMesaECadeiras004Persp,
+    JogoDeMesaECadeiras004Side,
+    JogoDeMesaECadeiras004Top,
+    LivingDivaniNeowallFront,
+    LivingDivaniNeowallPersp,
+    LivingDivaniNeowallSide,
+    LivingDivaniNeowallTop,
+    LivingDivaniFrogLoungeFront,
+    LivingDivaniFrogLoungePersp,
+    LivingDivaniFrogLoungeSide,
+    LivingDivaniFrogLoungeTop,
+    MDFItaliaEdgar2Front,
+    MDFItaliaEdgar2Persp,
+    MDFItaliaEdgar2Side,
+    MDFItaliaEdgar2Top,
+    MDFItaliaYaleFront,
+    MDFItaliaYalePersp,
+    MDFItaliaYaleSide,
+    MDFItaliaYaleTop,
+    MiniRefrigerador001Front,
+    MiniRefrigerador001Persp,
+    MiniRefrigerador001Side,
+    MiniRefrigerador001Top,
+    MiniRefrigerador002Front,
+    MiniRefrigerador002Persp,
+    MiniRefrigerador002Side,
+    MiniRefrigerador002Top,
+    LavaLoucas001Front,
+    LavaLoucas001Persp,
+    LavaLoucas001Side,
+    LavaLoucas001Top,
+    LavaRoupa001Front,
+    LavaRoupa001Persp,
+    LavaRoupa001Side,
+    LavaRoupa001Top,
+    Fogao001Front,
+    Fogao001Persp,
+    Fogao001Side,
+    Fogao001Top,
+    Forno001Front,
+    Forno001Persp,
+    Forno001Side,
+    Forno001Top,
+    Forno002Front,
+    Forno002Persp,
+    Forno002Side,
+    Forno002Top
+]
+
+// export {
+//     BancoExterno001Front,
+//     BancoExterno001Persp,
+//     BancoExterno001Side,
+//     BancoExterno001Top,
+//     BancoExterno002Front,
+//     BancoExterno002Persp,
+//     BancoExterno002Side,
+//     BancoExterno002Top,
+//     BancoExterno003Front,
+//     BancoExterno003Persp,
+//     BancoExterno003Side,
+//     BancoExterno003Top,
+//     BancoExterno004Front,
+//     BancoExterno004Persp,
+//     BancoExterno004Side,
+//     BancoExterno004Top,
+//     BancoExterno005Front,
+//     BancoExterno005Persp,
+//     BancoExterno005Side,
+//     BancoExterno005Top,
+//     Bike001Front,
+//     Bike001Persp,
+//     Bike001Side,
+//     Bike001Top,
+//     CalligarisSwingFront,
+//     CalligarisSwingPersp,
+//     CalligarisSwingSide,
+//     CalligarisSwingTop,
+//     Cooktop001Front,
+//     Cooktop001Persp,
+//     Cooktop001Side,
+//     Cooktop001Top,
+//     Cooktop002Front,
+//     Cooktop002Persp,
+//     Cooktop002Side,
+//     Cooktop002Top,
+//     DepuradorDeAr001Front,
+//     DepuradorDeAr001Persp,
+//     DepuradorDeAr001Side,
+//     DepuradorDeAr001Top,
+//     EmmegiEm202MeshFront,
+//     EmmegiEm202MeshPersp,
+//     EmmegiEm202MeshSide,
+//     EmmegiEm202MeshTop,
+//     FlexFormFront,
+//     FlexFormPersp,
+//     FlexFormSide,
+//     FlexFormTop,
+//     JogoDeMesaECadeiras001Front,
+//     JogoDeMesaECadeiras001Persp,
+//     JogoDeMesaECadeiras001Side,
+//     JogoDeMesaECadeiras001Top,
+//     JogoDeMesaECadeiras002Front,
+//     JogoDeMesaECadeiras002Persp,
+//     JogoDeMesaECadeiras002Side,
+//     JogoDeMesaECadeiras002Top,
+//     JogoDeMesaECadeiras003Front,
+//     JogoDeMesaECadeiras003Persp,
+//     JogoDeMesaECadeiras003Side,
+//     JogoDeMesaECadeiras003Top,
+//     JogoDeMesaECadeiras004Front,
+//     JogoDeMesaECadeiras004Persp,
+//     JogoDeMesaECadeiras004Side,
+//     JogoDeMesaECadeiras004Top,
+//     LivingDivaniNeowallFront,
+//     LivingDivaniNeowallPersp,
+//     LivingDivaniNeowallSide,
+//     LivingDivaniNeowallTop,
+//     LivingDivaniFrogLoungeFront,
+//     LivingDivaniFrogLoungePersp,
+//     LivingDivaniFrogLoungeSide,
+//     LivingDivaniFrogLoungeTop,
+//     MDFItaliaEdgar2Front,
+//     MDFItaliaEdgar2Persp,
+//     MDFItaliaEdgar2Side,
+//     MDFItaliaEdgar2Top,
+//     MDFItaliaYaleFront,
+//     MDFItaliaYalePersp,
+//     MDFItaliaYaleSide,
+//     MDFItaliaYaleTop,
+//     MiniRefrigerador001Front,
+//     MiniRefrigerador001Persp,
+//     MiniRefrigerador001Side,
+//     MiniRefrigerador001Top,
+//     MiniRefrigerador002Front,
+//     MiniRefrigerador002Persp,
+//     MiniRefrigerador002Side,
+//     MiniRefrigerador002Top,
+//     LavaLoucas001Front,
+//     LavaLoucas001Persp,
+//     LavaLoucas001Side,
+//     LavaLoucas001Top,
+//     LavaRoupa001Front,
+//     LavaRoupa001Persp,
+//     LavaRoupa001Side,
+//     LavaRoupa001Top,
+//     Fogao001Front,
+//     Fogao001Persp,
+//     Fogao001Side,
+//     Fogao001Top,
+//     Forno001Front,
+//     Forno001Persp,
+//     Forno001Side,
+//     Forno001Top,
+//     Forno002Front,
+//     Forno002Persp,
+//     Forno002Side,
+//     Forno002Top
+// }
--- src/pages/studio/index.jsx
+++ src/pages/studio/index.jsx
@@ -1,21 +1,107 @@
+import React, { useEffect, useState } from 'react';
 import Workspace from 'polotno/canvas/workspace';
 import { createStore } from 'polotno/model/store';
 import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
 import { Toolbar } from 'polotno/toolbar/toolbar';
 import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
 import { SidePanel } from 'polotno/side-panel';
+import { getImageSize } from 'polotno/utils/image';
+import { SectionTab } from 'polotno/side-panel';
+import { ImagesGrid } from 'polotno/side-panel/images-grid';
+import MdPhotoLibrary from '@meronex/icons/md/MdPhotoLibrary';
+import {
+    TextSection,
+    ElementsSection,
+    UploadSection,
+    BackgroundSection,
+    SizeSection,
+} from 'polotno/side-panel';
+import { observer } from 'mobx-react-lite';
+import { InputGroup } from '@blueprintjs/core';
+import { allImages } from '../../assets';
 
 import '@blueprintjs/core/lib/css/blueprint.css';
 
-const store = createStore();
+const store = createStore({
+    key: 'nFA5H9elEytDyPyvKL7T',
+    showCredit: true,
+});
 
 store.addPage();
+
+export const PhotosPanel = observer(({ store }) => {
+    const [images, setImages] = useState([]);
+
+    async function loadImages() {
+        setImages([]);
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        setImages(allImages.map((imgUrl) => (
+            { url: imgUrl }
+        )));
+    };
+
+    useEffect(() => {
+        loadImages();
+    }, []);
+
+    return (
+        <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <InputGroup
+                leftIcon="search"
+                placeholder="Search..."
+                onChange={(e) => {
+                    loadImages();
+                }}
+                style={{
+                    marginBottom: '20px',
+                }}
+            />
+            <ImagesGrid
+                images={images}
+                getPreview={(image) => image.url}
+                onSelect={async (image, pos) => {
+                    const { width, height } = await getImageSize(image.url);
+                    store.activePage.addElement({
+                        type: 'image',
+                        src: image.url,
+                        width,
+                        height,
+                        x: pos ? pos.x : store.width / 2 - width / 2,
+                        y: pos ? pos.y : store.height / 2 - height / 2,
+                    });
+                }}
+                rowsNumber={2}
+                isLoading={!images.length}
+                loadMore={false}
+            />
+        </div>
+    );
+});
+
+const CustomPhotos = {
+    name: 'photos',
+    Tab: (props) => (
+        <SectionTab name="Photos" {...props}>
+            <MdPhotoLibrary />
+        </SectionTab>
+    ),
+    Panel: PhotosPanel,
+};
+
+const sections = [
+    TextSection,
+    CustomPhotos,
+    ElementsSection,
+    UploadSection,
+    BackgroundSection,
+    SizeSection,
+];
 
 export default function Studio() {
     return (
         <PolotnoContainer style={{ width: '100vw', height: '100vh' }}>
             <SidePanelWrap>
-                <SidePanel store={store} />
+                <SidePanel store={store} sections={sections} defaultSection="photos" />
             </SidePanelWrap>
             <WorkspaceWrap>
                 <Toolbar store={store} />
@@ -24,4 +110,4 @@ export default function Studio() {
             </WorkspaceWrap>
         </PolotnoContainer>
     );
-}
+};
