01b7cf266cf6196891a06c8bb975adad0531e790
Felipe Baptista <felipe.baptista06@gmail.com>
2024-04-08 18:45:27 -0300

    Finish all images in polotno studio

--- src/assets/index.js
+++ src/assets/index.js
@@ -106,6 +106,98 @@ import MiniRefrigerador002Front from './imgs/Mini Refrigerador 002 - Front.png';
 import MiniRefrigerador002Persp from './imgs/Mini Refrigerador 002 - Persp.png';
 import MiniRefrigerador002Side from './imgs/Mini Refrigerador 002 - Side.png';
 import MiniRefrigerador002Top from './imgs/Mini Refrigerador 002 - Top.png';
+import MinottiAlisonIrokoFront from './imgs/Minotti Alison Iroko - Front.png';
+import MinottiAlisonIrokoPersp from './imgs/Minotti Alison Iroko - Persp.png';
+import MinottiAlisonIrokoSide from './imgs/Minotti Alison Iroko - Side.png';
+import MinottiAlisonIrokoTop from './imgs/Minotti Alison Iroko - Top.png';
+import Ombrelone001Front from './imgs/Ombrelone 001 - Front.png';
+import Ombrelone001Persp from './imgs/Ombrelone 001 - Persp.png';
+import Ombrelone001Side from './imgs/Ombrelone 001 - Side.png';
+import Ombrelone001Top from './imgs/Ombrelone 001 - Top.png';
+import Ombrelone002Front from './imgs/Ombrelone 002 - Front.png';
+import Ombrelone002Persp from './imgs/Ombrelone 002 - Persp.png';
+import Ombrelone002Side from './imgs/Ombrelone 002 - Side.png';
+import Ombrelone002Top from './imgs/Ombrelone 002 - Top.png';
+import Ombrelone002aFront from './imgs/Ombrelone 002a - Front.png';
+import Ombrelone002aPersp from './imgs/Ombrelone 002a - Persp.png';
+import Ombrelone002aSide from './imgs/Ombrelone 002a - Side.png';
+import Ombrelone002aTop from './imgs/Ombrelone 002a - Top.png';
+import PiaElevada001Front from './imgs/Pia Elevada 001 - Front.png';
+import PiaElevada001Persp from './imgs/Pia Elevada 001 - Persp.png';
+import PiaElevada001Side from './imgs/Pia Elevada 001 - Side.png';
+import PiaElevada001Top from './imgs/Pia Elevada 001 - Top.png';
+import PiaElevada002Front from './imgs/Pia Elevada 002 - Front.png';
+import PiaElevada002Persp from './imgs/Pia Elevada 002 - Persp.png';
+import PiaElevada002Side from './imgs/Pia Elevada 002 - Side.png';
+import PiaElevada002Top from './imgs/Pia Elevada 002 - Top.png';
+import PoliformCarmelFront from './imgs/Poliform Carmel - Front.png';
+import PoliformCarmelPersp from './imgs/Poliform Carmel - Persp.png';
+import PoliformCarmelSide from './imgs/Poliform Carmel - Side.png';
+import PoliformCarmelTop from './imgs/Poliform Carmel - Top.png';
+import PoliformMetropolitanFront from './imgs/Poliform Metropolitan - Front.png';
+import PoliformMetropolitanPersp from './imgs/Poliform Metropolitan - Persp.png';
+import PoliformMetropolitanSide from './imgs/Poliform Metropolitan - Side.png';
+import PoliformMetropolitanTop from './imgs/Poliform Metropolitan - Top.png';
+import PosteDeLuz001Front from './imgs/Poste de Luz 001 - Front.png';
+import PosteDeLuz001Persp from './imgs/Poste de Luz 001 - Persp.png';
+import PosteDeLuz001Side from './imgs/Poste de Luz 001 - Side.png';
+import PosteDeLuz001Top from './imgs/Poste de Luz 001 - Top.png';
+import PosteDeLuz002Front from './imgs/Poste de Luz 002 - Front.png';
+import PosteDeLuz002Persp from './imgs/Poste de Luz 002 - Persp.png';
+import PosteDeLuz002Side from './imgs/Poste de Luz 002 - Side.png';
+import PosteDeLuz002Top from './imgs/Poste de Luz 002 - Top.png';
+import Refrigerador001Front from './imgs/Refrigerador 001 - Front.png';
+import Refrigerador001Persp from './imgs/Refrigerador 001 - Persp.png';
+import Refrigerador001Side from './imgs/Refrigerador 001 - Side.png';
+import Refrigerador001Top from './imgs/Refrigerador 001 - Top.png';
+import RolfBenzMioFront from './imgs/Rolf Benz Mio - Front.png';
+import RolfBenzMioPersp from './imgs/Rolf Benz Mio - Persp.png';
+import RolfBenzMioSide from './imgs/Rolf Benz Mio - Side.png';
+import RolfBenzMioTop from './imgs/Rolf Benz Mio - Top.png';
+import SerralungaLazyFront from './imgs/Serralunga Lazy - Front.png';
+import SerralungaLazyPersp from './imgs/Serralunga Lazy - Persp.png';
+import SerralungaLazySide from './imgs/Serralunga Lazy - Side.png';
+import SerralungaLazyTop from './imgs/Serralunga Lazy - Top.png';
+import Vaso001Front from './imgs/Vaso 001 - Front.png';
+import Vaso001Persp from './imgs/Vaso 001 - Persp.png';
+import Vaso001Side from './imgs/Vaso 001 - Side.png';
+import Vaso001Top from './imgs/Vaso 001 - Top.png';
+import Vaso002Front from './imgs/Vaso 002 - Front.png';
+import Vaso002Persp from './imgs/Vaso 002 - Persp.png';
+import Vaso002Top from './imgs/Vaso 002 - Top.png';
+import Vaso003Front from './imgs/Vaso 003 - Front.png';
+import Vaso003Persp from './imgs/Vaso 003 - Persp.png';
+import Vaso003Side from './imgs/Vaso 003 - Side.png';
+import Vaso003Top from './imgs/Vaso 003 - Top.png';
+import Vaso004Front from './imgs/Vaso 004 - Front.png';
+import Vaso004Persp from './imgs/Vaso 004 - Persp.png';
+import Vaso004Side from './imgs/Vaso 004 - Side.png';
+import Vaso004Top from './imgs/Vaso 004 - Top.png';
+import Vaso005Front from './imgs/Vaso 005 - Front.png';
+import Vaso005Persp from './imgs/Vaso 005 - Persp.png';
+import Vaso005Side from './imgs/Vaso 005 - Side.png';
+import Vaso005Top from './imgs/Vaso 005 - Top.png';
+import Vaso006Front from './imgs/Vaso 006 - Front.png';
+import Vaso006Persp from './imgs/Vaso 006 - Persp.png';
+import Vaso006Side from './imgs/Vaso 006 - Side.png';
+import Vaso006Top from './imgs/Vaso 006 - Top.png';
+import Vaso021Side from './imgs/Vaso 021 - Side.png';
+import VitraEA117Front from './imgs/Vitra - EA117 - Front.png';
+import VitraEA117Persp from './imgs/Vitra - EA117 - Persp.png';
+import VitraEA117Side from './imgs/Vitra - EA117 - Side.png';
+import VitraEA117Top from './imgs/Vitra - EA117 - Top.png';
+import VitraEagamesPlasticFront from './imgs/Vitra - Eames Plastic Armchair Dar - Front.png';
+import VitraEagamesPlasticPersp from './imgs/Vitra - Eames Plastic Armchair Dar - Persp.png';
+import VitraEagamesPlasticSide from './imgs/Vitra - Eames Plastic Armchair Dar - Side.png';
+import VitraEagamesPlasticTop from './imgs/Vitra - Eames Plastic Armchair Dar - Top.png';
+import VitrasEA215Front from './imgs/Vitras - EA 215 - Front.png';
+import VitrasEA215Persp from './imgs/Vitras - EA 215 - Persp.png';
+import VitrasEA215Side from './imgs/Vitras - EA 215 - Side.png';
+import VitrasEA215Top from './imgs/Vitras - EA 215 - Top.png';
+import ZeitraumFront from './imgs/Zeitraum Morph Lounge - Front.png';
+import ZeitraumPersp from './imgs/Zeitraum Morph Lounge - Persp.png';
+import ZeitraumSide from './imgs/Zeitraum Morph Lounge - Side.png';
+import ZeitraumTop from './imgs/Zeitraum Morph Lounge - Top.png';
 
 export const allImages = [
     BancoExterno001Front,
@@ -156,6 +248,18 @@ export const allImages = [
     FlexFormPersp,
     FlexFormSide,
     FlexFormTop,
+    Fogao001Front,
+    Fogao001Persp,
+    Fogao001Side,
+    Fogao001Top,
+    Forno001Front,
+    Forno001Persp,
+    Forno001Side,
+    Forno001Top,
+    Forno002Front,
+    Forno002Persp,
+    Forno002Side,
+    Forno002Top,
     JogoDeMesaECadeiras001Front,
     JogoDeMesaECadeiras001Persp,
     JogoDeMesaECadeiras001Side,
@@ -172,6 +276,14 @@ export const allImages = [
     JogoDeMesaECadeiras004Persp,
     JogoDeMesaECadeiras004Side,
     JogoDeMesaECadeiras004Top,
+    LavaLoucas001Front,
+    LavaLoucas001Persp,
+    LavaLoucas001Side,
+    LavaLoucas001Top,
+    LavaRoupa001Front,
+    LavaRoupa001Persp,
+    LavaRoupa001Side,
+    LavaRoupa001Top,
     LivingDivaniNeowallFront,
     LivingDivaniNeowallPersp,
     LivingDivaniNeowallSide,
@@ -196,135 +308,96 @@ export const allImages = [
     MiniRefrigerador002Persp,
     MiniRefrigerador002Side,
     MiniRefrigerador002Top,
-    LavaLoucas001Front,
-    LavaLoucas001Persp,
-    LavaLoucas001Side,
-    LavaLoucas001Top,
-    LavaRoupa001Front,
-    LavaRoupa001Persp,
-    LavaRoupa001Side,
-    LavaRoupa001Top,
-    Fogao001Front,
-    Fogao001Persp,
-    Fogao001Side,
-    Fogao001Top,
-    Forno001Front,
-    Forno001Persp,
-    Forno001Side,
-    Forno001Top,
-    Forno002Front,
-    Forno002Persp,
-    Forno002Side,
-    Forno002Top
-]
-
-// export {
-//     BancoExterno001Front,
-//     BancoExterno001Persp,
-//     BancoExterno001Side,
-//     BancoExterno001Top,
-//     BancoExterno002Front,
-//     BancoExterno002Persp,
-//     BancoExterno002Side,
-//     BancoExterno002Top,
-//     BancoExterno003Front,
-//     BancoExterno003Persp,
-//     BancoExterno003Side,
-//     BancoExterno003Top,
-//     BancoExterno004Front,
-//     BancoExterno004Persp,
-//     BancoExterno004Side,
-//     BancoExterno004Top,
-//     BancoExterno005Front,
-//     BancoExterno005Persp,
-//     BancoExterno005Side,
-//     BancoExterno005Top,
-//     Bike001Front,
-//     Bike001Persp,
-//     Bike001Side,
-//     Bike001Top,
-//     CalligarisSwingFront,
-//     CalligarisSwingPersp,
-//     CalligarisSwingSide,
-//     CalligarisSwingTop,
-//     Cooktop001Front,
-//     Cooktop001Persp,
-//     Cooktop001Side,
-//     Cooktop001Top,
-//     Cooktop002Front,
-//     Cooktop002Persp,
-//     Cooktop002Side,
-//     Cooktop002Top,
-//     DepuradorDeAr001Front,
-//     DepuradorDeAr001Persp,
-//     DepuradorDeAr001Side,
-//     DepuradorDeAr001Top,
-//     EmmegiEm202MeshFront,
-//     EmmegiEm202MeshPersp,
-//     EmmegiEm202MeshSide,
-//     EmmegiEm202MeshTop,
-//     FlexFormFront,
-//     FlexFormPersp,
-//     FlexFormSide,
-//     FlexFormTop,
-//     JogoDeMesaECadeiras001Front,
-//     JogoDeMesaECadeiras001Persp,
-//     JogoDeMesaECadeiras001Side,
-//     JogoDeMesaECadeiras001Top,
-//     JogoDeMesaECadeiras002Front,
-//     JogoDeMesaECadeiras002Persp,
-//     JogoDeMesaECadeiras002Side,
-//     JogoDeMesaECadeiras002Top,
-//     JogoDeMesaECadeiras003Front,
-//     JogoDeMesaECadeiras003Persp,
-//     JogoDeMesaECadeiras003Side,
-//     JogoDeMesaECadeiras003Top,
-//     JogoDeMesaECadeiras004Front,
-//     JogoDeMesaECadeiras004Persp,
-//     JogoDeMesaECadeiras004Side,
-//     JogoDeMesaECadeiras004Top,
-//     LivingDivaniNeowallFront,
-//     LivingDivaniNeowallPersp,
-//     LivingDivaniNeowallSide,
-//     LivingDivaniNeowallTop,
-//     LivingDivaniFrogLoungeFront,
-//     LivingDivaniFrogLoungePersp,
-//     LivingDivaniFrogLoungeSide,
-//     LivingDivaniFrogLoungeTop,
-//     MDFItaliaEdgar2Front,
-//     MDFItaliaEdgar2Persp,
-//     MDFItaliaEdgar2Side,
-//     MDFItaliaEdgar2Top,
-//     MDFItaliaYaleFront,
-//     MDFItaliaYalePersp,
-//     MDFItaliaYaleSide,
-//     MDFItaliaYaleTop,
-//     MiniRefrigerador001Front,
-//     MiniRefrigerador001Persp,
-//     MiniRefrigerador001Side,
-//     MiniRefrigerador001Top,
-//     MiniRefrigerador002Front,
-//     MiniRefrigerador002Persp,
-//     MiniRefrigerador002Side,
-//     MiniRefrigerador002Top,
-//     LavaLoucas001Front,
-//     LavaLoucas001Persp,
-//     LavaLoucas001Side,
-//     LavaLoucas001Top,
-//     LavaRoupa001Front,
-//     LavaRoupa001Persp,
-//     LavaRoupa001Side,
-//     LavaRoupa001Top,
-//     Fogao001Front,
-//     Fogao001Persp,
-//     Fogao001Side,
-//     Fogao001Top,
-//     Forno001Front,
-//     Forno001Persp,
-//     Forno001Side,
-//     Forno001Top,
-//     Forno002Front,
-//     Forno002Persp,
-//     Forno002Side,
-//     Forno002Top
-// }
+    MinottiAlisonIrokoFront,
+    MinottiAlisonIrokoPersp,
+    MinottiAlisonIrokoSide,
+    MinottiAlisonIrokoTop,
+    Ombrelone001Front,
+    Ombrelone001Persp,
+    Ombrelone001Side,
+    Ombrelone001Top,
+    Ombrelone002Front,
+    Ombrelone002Persp,
+    Ombrelone002Side,
+    Ombrelone002Top,
+    Ombrelone002aFront,
+    Ombrelone002aPersp,
+    Ombrelone002aSide,
+    Ombrelone002aTop,
+    PiaElevada001Front,
+    PiaElevada001Persp,
+    PiaElevada001Side,
+    PiaElevada001Top,
+    PiaElevada002Front,
+    PiaElevada002Persp,
+    PiaElevada002Side,
+    PiaElevada002Top,
+    PoliformCarmelFront,
+    PoliformCarmelPersp,
+    PoliformCarmelSide,
+    PoliformCarmelTop,
+    PoliformMetropolitanFront,
+    PoliformMetropolitanPersp,
+    PoliformMetropolitanSide,
+    PoliformMetropolitanTop,
+    PosteDeLuz001Front,
+    PosteDeLuz001Persp,
+    PosteDeLuz001Side,
+    PosteDeLuz001Top,
+    PosteDeLuz002Front,
+    PosteDeLuz002Persp,
+    PosteDeLuz002Side,
+    PosteDeLuz002Top,
+    Refrigerador001Front,
+    Refrigerador001Persp,
+    Refrigerador001Side,
+    Refrigerador001Top,
+    RolfBenzMioFront,
+    RolfBenzMioPersp,
+    RolfBenzMioSide,
+    RolfBenzMioTop,
+    SerralungaLazyFront,
+    SerralungaLazyPersp,
+    SerralungaLazySide,
+    SerralungaLazyTop,
+    Vaso001Front,
+    Vaso001Persp,
+    Vaso001Side,
+    Vaso001Top,
+    Vaso002Front,
+    Vaso002Persp,
+    Vaso002Top,
+    Vaso003Front,
+    Vaso003Persp,
+    Vaso003Side,
+    Vaso003Top,
+    Vaso004Front,
+    Vaso004Persp,
+    Vaso004Side,
+    Vaso004Top,
+    Vaso005Front,
+    Vaso005Persp,
+    Vaso005Side,
+    Vaso005Top,
+    Vaso006Front,
+    Vaso006Persp,
+    Vaso006Side,
+    Vaso006Top,
+    Vaso021Side,
+    VitraEA117Front,
+    VitraEA117Persp,
+    VitraEA117Side,
+    VitraEA117Top,
+    VitraEagamesPlasticFront,
+    VitraEagamesPlasticPersp,
+    VitraEagamesPlasticSide,
+    VitraEagamesPlasticTop,
+    VitrasEA215Front,
+    VitrasEA215Persp,
+    VitrasEA215Side,
+    VitrasEA215Top,
+    ZeitraumFront,
+    ZeitraumPersp,
+    ZeitraumSide,
+    ZeitraumTop,
+];
